9488eb21511c82ee8917a595336b26597daa944f
sagetony <uchetony224@gmail.com>
2023-04-30 00:12:06 +0100

    updated the test script

--- contracts/Soulbound.sol
+++ contracts/Soulbound.sol
@@ -1,14 +1,24 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.18;
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract Soulbound is ERC721{
+contract Soulbound is IERC721{
 
     uint8 public constant SUPPLY = 20;
     address public owner;
     uint8 public nftCount;
 
-    constructor () ERC721 ("SOULBOUNDTOKEN", "SBT"){
+    string public override name = "SOULBOUNDTOKEN";
+    string public override symbol = "SBT";
+    uint256 public override totalSupply;
+    mapping(address => uint256) public override balanceOf;
+    mapping(uint256 => address) public override ownerOf;
+    mapping(address => mapping(address => bool)) public override isApprovedForAll;
+    mapping(uint256 => mapping(address => bool)) public override isApproved;
+
+
+    constructor () {
         owner = msg.sender;
     }
     modifier onlyOwner () {
@@ -19,7 +29,23 @@ contract Soulbound is ERC721{
         require (nftCount < SUPPLY, "Maximum supply reached");
         ++nftCount;
         uint256 tokenId = nftCount;
-        _mint(_to, tokenId);
+        mint(_to, tokenId);
         return tokenId;
+    }
+       function mint(address _to, uint256 _tokenId) public  virtual  {}
+
+    function approve(address _approved, uint256 _tokenId) public virtual override {
+    }
+
+    function setApprovalForAll(address _operator, bool _approved) public virtual override {
+    }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) public virtual override {
+    }
+
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes memory _data) public virtual override {
+    }
+
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId) public virtual override {
     }
 }
