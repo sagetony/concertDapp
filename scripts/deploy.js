da65115831ee85a97f9c0379c4fc9c6411415965
sagetony <uchetony224@gmail.com>
2023-04-29 17:06:52 +0100

    written deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,15 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
-
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contract with the account:", deployer.address);
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  // Get the contract Factory
+  const DavidoConcert = await ethers.getContractFactory("DavidoConcert");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  // Depoly the contract
+  const davidoconcert = await DavidoConcert.deploy();
+  console.log("Deloyed contract address:", davidoconcert.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
