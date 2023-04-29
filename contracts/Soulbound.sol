96e46fbede403553880ffa7992411c394e729a80
sagetony <uchetony224@gmail.com>
2023-04-30 00:14:31 +0100

    updated the contract

--- contracts/Soulbound.sol
+++ contracts/Soulbound.sol
@@ -1,24 +1,14 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.18;
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Soulbound is IERC721{
+contract Soulbound is ERC721{
 
     uint8 public constant SUPPLY = 20;
     address public owner;
     uint8 public nftCount;
 
-    string public override name = "SOULBOUNDTOKEN";
-    string public override symbol = "SBT";
-    uint256 public override totalSupply;
-    mapping(address => uint256) public override balanceOf;
-    mapping(uint256 => address) public override ownerOf;
-    mapping(address => mapping(address => bool)) public override isApprovedForAll;
-    mapping(uint256 => mapping(address => bool)) public override isApproved;
-
-
-    constructor () {
+    constructor () ERC721 ("SOULBOUNDTOKEN", "SBT"){
         owner = msg.sender;
     }
     modifier onlyOwner () {
@@ -29,23 +19,7 @@ contract Soulbound is IERC721{
         require (nftCount < SUPPLY, "Maximum supply reached");
         ++nftCount;
         uint256 tokenId = nftCount;
-        mint(_to, tokenId);
+        _mint(_to, tokenId);
         return tokenId;
-    }
-       function mint(address _to, uint256 _tokenId) public  virtual  {}
-
-    function approve(address _approved, uint256 _tokenId) public virtual override {
-    }
-
-    function setApprovalForAll(address _operator, bool _approved) public virtual override {
-    }
-
-    function transferFrom(address _from, address _to, uint256 _tokenId) public virtual override {
-    }
-
-    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes memory _data) public virtual override {
-    }
-
-    function safeTransferFrom(address _from, address _to, uint256 _tokenId) public virtual override {
     }
 }
